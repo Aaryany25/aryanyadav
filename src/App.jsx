af16e5907a9bfa1182ee9080c371b9ac96389284
Aryan Yadav <aaryany2505@gmail.com>
2026-06-15 00:32:56 +0530

    Added Gradient in background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 import bg from "./assets/bg.jpg"
 import GithubMap from "./components/GithubCalender"
 import Hero from "./components/Hero"
+import "@fontsource/geist"; // Defaults to weight 400
+
 function App() {
 
   return (
    <div className="bg-black min-h-screen text-white ">
 <main className="max-w-xl mx-auto py-10 relative">
   <div className="relative">
-    <img src={bg} alt="Background" className="w-full h-54 object-cover rounded"/>
+    <img src={bg} alt="Background" className="w-full h-54 object-cover rounded "/>
+     <div className="absolute inset-x-0 bottom-0 h-24 bg-gradient-to-t from-black to-transparent rounded-b" />
     <Hero/>
   </div>
 
-  <div className="mt-20 max-w-prose text-gray-300 leading-relaxed">
+  <div className="mt-16 max-w-prose text-gray-300 leading-relaxed mb-5">
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, facilis nostrum? Dignissimos, enim placeat quae libero fuga optio. Deleniti, nesciunt.
   </div>
-  <div className="mt-10">
+  <hr></hr>
+  <div className="mt-5 mb-5">
     <GithubMap/>
   </div>
+   <hr></hr>
 </main>
 
    </div>
