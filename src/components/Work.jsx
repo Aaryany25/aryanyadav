9bb5f66722bc0ce2a152c92781be9df7504eaa80
Aryan Yadav <aaryany2505@gmail.com>
2026-09-22 23:07:59 +0530

    updated the Roles and Responsiblity

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -32,6 +32,7 @@ function Work() {
           <p>• Leading product direction, engineering, and open-source contributions.</p>
           <p>• Shipping features across web, backend, and AI integrations end-to-end.</p>
           <p>• Built automation tools to automate internal systems and workflows...</p>
+          <p>• Managed 15+ clients          </p>
         </div>
       )}
     </>
